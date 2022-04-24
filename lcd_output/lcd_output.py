655a72a542e0f6288b6ba39a7b6a04be16778006
teo_v <teodantes@gmail.com>
2022-04-24 17:59:56 +0300

    Make the package script executed

--- lcd_output/lcd_output.py
+++ lcd_output/lcd_output.py
@@ -22,26 +22,28 @@ def is_integer():
         print('\nTry again!\n')
         is_integer()
 
+def main():
+    print('\nPlease, enter any integer:\n')
+    is_integer()
 
-print('\nPlease, enter any integer:\n')
-is_integer()
+    input_string_len = len(input_string)
+    horizontal_line = 'x' + '-' * (4 * input_string_len + input_string_len - 1) + 'x'
 
-input_string_len = len(input_string)
-horizontal_line = 'x' + '-' * (4 * input_string_len + input_string_len - 1) + 'x'
+    print()
+    print('RESULT:\n')
+    print(horizontal_line)
+    for _ in range(0, 7):
+        print('|', end='')
+        for symbol in input_string:
+            if symbol is input_string[-1]:
+                print(glyph(int(symbol), _), end='')
+            else:
+                print(glyph(int(symbol), _) + ' ', end='')
+        print('|')
+    print(horizontal_line)
+    print()
 
-print()
-print('RESULT:\n')
-print(horizontal_line)
-for _ in range(0,7):
-    print('|', end='')
-    for symbol in input_string:
-        if symbol is input_string[-1]:
-            print(glyph(int(symbol), _), end='')
-        else:
-            print(glyph(int(symbol), _) + ' ', end='')
-    print('|')
-print(horizontal_line)
-print()
-
+if __name__ == '__main__':
+    main()
 
 
